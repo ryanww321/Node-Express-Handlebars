5c307cecd47325d1819621d7c48b2146486dedab
Ryan Whitcomb <ryanww321@gmail.com>
2020-03-01 13:01:02 -0600

    added jawsdb support

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
-const mysql = require("mysql");
+var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "rootroot",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+
+}
+else {
+    var connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "",
+        database: "burgers_db"
+    });
+}
 
 connection.connect(function (err) {
     if (err) {
