aa385e4749e1068fbeafb192f5b9f1d50bb52aed
Ryan Whitcomb <ryanww321@gmail.com>
2020-03-01 12:58:07 -0600

    updated orm js, added burger img

--- config/orm.js
+++ config/orm.js
@@ -1,78 +1,66 @@
-// const connection = require("./connection");
+var connection = require("../config/connection.js");
 
-// function printQuestionMarks(num) {
-//     var arr = [];
+//Make sets work properly
+function objToSql(ob) {
+    var arr = [];
+    for (var key in ob) {
+        var value = ob[key];
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
+    return arr.toString();
+}
 
-//     for (var i = 0; i < num; i++) {
-//         arr.push("?");
-//     }
+var orm = {
+    selectAll: function (tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    insertOne: function (tableInput, set, cb) {
+        var queryString = "INSERT INTO " + tableInput;
+        queryString += " SET " + objToSql(set);
+        connection.query(queryString, (err, result) => {
+            if (err) throw err;
+            cb(result);
+        })
+    },
+    updateOne: function (tableInput, set, condition, cb) {
+        var queryString = "UPDATE " + tableInput;
 
-//     return arr.toString();
-// }
+        queryString += " SET ";
+        queryString += objToSql(set);
+        queryString += " WHERE ";
+        queryString += condition;
 
-// function objToSql(ob) {
-//     var arr = [];
+        console.log(queryString);
+        connection.query(queryString, function (err, result) {
+            if (err) throw err;
 
-//     for (var key in ob) {
-//         var value = ob[key];
-//         if (Object.hasOwnProperty.call(ob, key)) {
-//             if (typeof value === "string" && value.indexOf(" ") >= 0) {
-//                 value = "'" + value + "'";
-//             }
-//             arr.push(key + "=" + value);
-//         }
-//     }
+            cb(result);
+        });
 
-//     return arr.toString();
-// }
+    },
+    deleteOne: function (tableInput, condition, cb) {
+        var queryString = "DELETE FROM " + tableInput;
+        queryString += " WHERE ";
+        queryString += condition;
 
-// var orm = {
-//     all: function (tableInput, cb) {
-//         var queryString = "SELECT * FROM " + tableInput + ";";
-//         connection.query(queryString, function (err, result) {
-//             if (err) {
-//                 throw err;
-//             }
-//             cb(result);
-//         });
-//     },
-//     create: function (table, cols, vals, cb) {
-//         var queryString = "INSERT INTO " + table;
+        console.log(queryString);
+        connection.query(queryString, function (err, result) {
+            if (err) throw err;
+            cb(result);
+        })
+    }
 
-//         queryString += " (";
-//         queryString += cols.toString();
-//         queryString += ") ";
-//         queryString += "VALUES (";
-//         queryString += printQuestionMarks(vals.length);
-//         queryString += ") ";
+};
 
-//         console.log(queryString);
-
-//         connection.query(queryString, vals, function (err, result) {
-//             if (err) {
-//                 throw err;
-//             }
-
-//             cb(result);
-//         });
-//     },
-//     update: function (table, objColVals, condition, cb) {
-//         var queryString = "UPDATE " + table;
-
-//         queryString += " SET ";
-//         queryString += objToSql(objColVals);
-//         queryString += " WHERE ";
-//         queryString += condition;
-
-//         console.log(queryString);
-//         connection.query(queryString, function (err, result) {
-//             if (err) {
-//                 throw err;
-//             }
-
-//             cb(result);
-//         });
-//     }
-// };
-
-// module.exports = orm;
+module.exports = orm;
